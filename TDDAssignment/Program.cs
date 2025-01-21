f0776edbba3b14e7e1dde540293de9962fb0ccf9
99amiqur <99amiqur@gafe.molndal.se>
2025-01-21 10:25:32 +0100

    first commit

---
+++ TDDAssignment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TDDAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("First push to Git");
+        }
+    }
+}
